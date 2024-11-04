e3f729291403d0a74397c0487c75b4fb191ce23d
BrunoSantos <santosbdev@gmail.com>
2024-11-04 19:14:01 -0300

    adicionado tela de game over

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function posicaoAleatoria() {
         document.getElementById('mosquito').remove()
 
         if(vidas > 3) {
-            alert('Game Over')
+            window.location.href = 'fim_de_jogo.html'
         } else {
             document.querySelector('#v' + vidas).src = "/imagens/coracao_vazio.png"
             vidas++
